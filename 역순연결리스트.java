726765480d31b0f0e942b5513a57f29019b19df7
ksdhk <47198182+ksdhk@users.noreply.github.com>
2024-03-22 01:54:42 +0900

    Update 역순연결리스트.java

--- 역순연결리스트.java
+++ 역순연결리스트.java
@@ -14,11 +14,13 @@ class Solution {
 
         while (current != null) {
             // (#null->1->2->3->4->5->#null)
-            //  prev=>cur=>
+            //  prev=>cur=>         
             next = current.next; //다음노드값 지정              
             current.next = prev; //현재노드의 다음으로 prev노드 지정 
             prev = current;   //이전노드는 현재 노드로 지정  
             current = next;   //지정한 next의 다음노드를 현재노드로지정
+            // (#null<-1<-2<-3<-4<-5<-#null)
+            //                   prev=>cur=>
         }
         
         return prev;
