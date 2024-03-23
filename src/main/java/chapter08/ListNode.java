7987c5af1d5cd76f31cec78f695d58ded16968c1
YoonSung Kim <opqudk@gmail.com>
2024-03-24 08:40:29 +0900

    fix: 유틸 -> 메소드

--- src/main/java/chapter08/ListNode.java
+++ src/main/java/chapter08/ListNode.java
@@ -12,4 +12,40 @@ public class ListNode {
         this.val = val;
         this.next = next;
     }
+
+    public static ListNode of(int... values) {
+        ListNode head = null;
+        ListNode tail = null;
+
+        for (int val : values) {
+            ListNode newNode = new ListNode(val);
+
+            if (head == null) {
+                head = newNode;
+            } else {
+                tail.next = newNode;
+            }
+
+            tail = newNode;
+        }
+
+        return head;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ListNode list2)) return false;
+
+        ListNode list1 = this;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val != list2.val) return false;
+
+            list1 = list1.next;
+            list2 = list2.next;
+        }
+
+        return list1 == null && list2 == null;
+    }
 }
--- src/test/java/chapter08/SwapNodesInPairsTest.java
+++ src/test/java/chapter08/SwapNodesInPairsTest.java
@@ -1,13 +1,14 @@
 package chapter08;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SwapNodesInPairsTest {
     @Test
     public void test() {
-        ListNode input = Utils.arrayToList(new int[]{1, 2, 3, 4, 5, 6});
-        int[] answer = {2, 1, 4, 3, 6, 5};
-        
-        Utils.assertListEqualsArray(SwapNodesInPairs.solution(input), answer);
+        ListNode input = ListNode.of(1, 2, 3, 4, 5, 6);
+        ListNode answer = ListNode.of(2, 1, 4, 3, 6, 5);
+
+        Assertions.assertEquals(answer, SwapNodesInPairs.solution(input));
     }
 }
