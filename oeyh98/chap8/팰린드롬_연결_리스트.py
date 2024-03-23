9a375026d010370d5659a8dd12042fea7724476d
oeyh98 <skusus123@gmail.com>
2024-03-24 00:01:15 +0900

    oeyh_sove8

---
+++ oeyh98/chap8/팰린드롬_연결_리스트.py
@@ -0,0 +1,27 @@
+import collections
+from typing import Deque
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        dq = collections.deque()
+
+        if not head:
+            return True
+
+        while head:
+            dq.append(head.val)
+            head = head.next
+
+        while len(dq) > 1:
+            if dq.popleft() != dq.pop():
+                return False
+
+        return True
