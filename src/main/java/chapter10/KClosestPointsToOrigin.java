122e3f65c1142b6c8f1a4169b3ef3fddd9e00178
YoonSung Kim <opqudk@gmail.com>
2024-04-04 16:50:53 +0900

    fix: stream -> priorityQueue

--- src/main/java/chapter10/KClosestPointsToOrigin.java
+++ src/main/java/chapter10/KClosestPointsToOrigin.java
@@ -1,12 +1,26 @@
 package chapter10;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.PriorityQueue;
 
 public class KClosestPointsToOrigin {
     public static int[][] solution(int[][] points, int k) {
-        return Arrays.stream(points)
-                .sorted(Comparator.comparingLong(point -> (long) point[0] * point[0] + (long) point[1] * point[1]))
-                .limit(k)
-                .toArray(int[][]::new);
+        if (points == null || points.length == 0 || k <= 0) return null;
+
+        PriorityQueue<Map<String, Object>> queue = new PriorityQueue<>(Comparator.comparingLong(m -> (long) m.get("distance")));
+
+        for (int[] point : points) {
+            Long distance = (long) point[0] * point[0] + (long) point[1] * point[1];
+            queue.add(Map.of("point", point, "distance", distance));
+        }
+
+        int[][] res = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            res[i] = (int[]) Objects.requireNonNull(queue.poll()).get("point");
+        }
+
+        return res;
     }
 }
